2963a23c6def147010fcc2406c183783ef77d125
Thomas <iato112024@gmail.com>
2024-11-20 10:41:16 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Info")
+
+// Variabler 
+
+// let x = 5
+// console.log(y)
+
+// I javascript bruker vi camelCase
+
+const introHeader = document.getElementById("intro-header")
+console.log(introHeader)
+
+// Datatyper
+
+// String
+let string = "Tekst"
+
+// Integer/Number
+let number = 10
+
+// Float
+let float = 10.5
+
+// Boolean = true
+console.log(boolean)
+
+let darkModeToggle = false
+
+// Array
+const fruits =["Apple", "Orange", "Pear", "Banana"]
+console.log(fruits)
+
+const arrayNumbers =["18", 15, 11.7] 
+console.log(arrayNumbers)
+
+// Object
+const person = {
+    firstName: "Thomas",
+    lastName: "Iversen",
+    age: 20,
+    hobbies: ["Piano", "Guitar", "Gaming"]
+}
+console.log(person)
